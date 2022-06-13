58a2f9c154eb13d470cef8da9ba8349d6e4c3a11
fahrianrama <fahrianramaditiya7@gmail.com>
2022-06-13 11:17:01 +0700

    commit on main

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,10 @@ const sendVerificationCode = require('../../services/mail.js');
 
 const router = express.Router();
 
+let user = {
+    id: null,
+}
+
 router.get('/', (req, res) => {
     // get all user data from database
     connection.query('SELECT * FROM users', (err, rows) => {
@@ -24,15 +28,12 @@ router.post('/auth', (req, res) => {
         } else {
             if (rows.length > 0) {
                 if (rows[0].password === req.body.password) {
+                    user.id = rows[0].user_id;
                     res.json({
                         success: true,
                         message: 'Authentication successful!',
                         user: rows[0]
                     });
-                    // set session for user
-                    req.session.user = rows[0];
-                    // send session
-                    res.send(req.session);
                 } else {
                     res.status(401).json({
                         success: false,
@@ -50,6 +51,19 @@ router.post('/auth', (req, res) => {
 }
 );
 
+// logout
+router.get('/logout', (req, res) => {
+    user.id = null;
+    res.json({
+        success: true,
+        message: 'Logout successful!'
+    });
+});
+
+router.get('/getid', (req, res) => {
+    res.json(user.id);
+});
+
 router.get('/:id', (req, res) => {
     // get user data from database
     connection.query('SELECT * FROM users WHERE user_id = ?', [req.params.id], (err, rows) => {
@@ -65,18 +79,43 @@ router.get('/:id', (req, res) => {
 router.post('/store', (req, res) => {
     // add new user to database
     const user = req.body;
-    // create random verification code
-    const verificationCode = Math.floor(Math.random() * 1000000);
-    // send verification code to user's email
-    sendVerificationCode(user.email, user.username, verificationCode);
-    connection.query(`INSERT INTO users (email, password, role, verification) VALUES ('${user.email}', '${user.password}', '${user.role}', '${verificationCode}')`, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(result);
+    // check req body not empty
+    if (user.email && user.password && user.role) {
+        // check if email already exists
+        connection.query('SELECT * FROM users WHERE email = ?', [user.email], (err, rows) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                if (rows.length > 0) {
+                    res.status(401).json({
+                        success: false,
+                        message: 'Email already exists!'
+                    });
+                } else {
+                    // add new user to database
+                    // create random verification code
+                    const verificationCode = Math.floor(Math.random() * 1000000);
+                    // send verification code to user's email
+                    sendVerificationCode(user.email, user.username, verificationCode);
+                    connection.query(`INSERT INTO users (email, password, role, verification) VALUES ('${user.email}', '${user.password}', '${user.role}', '${verificationCode}')`, (err, result) => {
+                        if (err) {
+                            res.status(500).send(err);
+                        } else {
+                            res.json(result);
+                        }
+                    }
+                    );
+                }
+            }
         }
+        );
     }
-    );
+    else {
+        res.status(401).json({
+            success: false,
+            message: 'Please fill in all fields!'
+        });
+    }
 });
 
 // update data of user in database
