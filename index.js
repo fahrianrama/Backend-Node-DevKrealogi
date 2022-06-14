e6d2ded358f3e9c09ec61fc91df8d96e71e21b0e
fahrianrama <fahrianramaditiya7@gmail.com>
2022-06-14 18:18:50 +0700

    commit on main

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // use all user route
 app.use('/api/users', Users);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // view index.html
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
