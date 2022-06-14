5aa600fe916ecafd3a6894cf3a1bdf5901192ef4
fahrianrama <fahrianramaditiya7@gmail.com>
2022-06-14 18:40:29 +0700

    update main

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/users', Users);
 
 // enable cors
-app.use(cors({
-    origin: '*'
-}));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://krealogi.netlify.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-  
+app.use(cors());
 
 // view index.html
 app.get('/', (req, res) => {
